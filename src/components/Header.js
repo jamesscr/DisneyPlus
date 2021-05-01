9f12b6fe93ea63e9b5f860428895f489d5b01dd0
jamesscr <allanjeanjacques@hotmail.com>
2021-05-01 19:05:48 -0400

    add DetailPage

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
 import styled from 'styled-components'
 function Header() {
@@ -6,27 +7,27 @@ function Header() {
             <Logo src='/images/logo.svg' />
             <NavMenu>
                 <a>
-                    <img src='/images/home-icon.svg' alt="home" />
+                    <img src='/images/home-icon.svg' alt="home-icon" />
                     <span>HOME</span>
                 </a>
                 <a>
-                    <img src='/images/search-icon.svg' alt="images" />
+                    <img src='/images/search-icon.svg' alt="search-icon" />
                     <span>SEARCH</span>
                 </a>
                 <a>
-                    <img src='/images/watchlist-icon.svg' alt="images" />
+                    <img src='/images/watchlist-icon.svg' alt="watchlist-icon" />
                     <span>WATCHLIST</span>
                 </a>
                 <a>
-                    <img src='/images/original-icon.svg' alt="images" />
+                    <img src='/images/original-icon.svg' alt="original-icon" />
                     <span>ORIGINALS</span>
                 </a>
                 <a>
-                    <img src='/images/movie-icon.svg' alt="images" />
+                    <img src='/images/movie-icon.svg' alt="movie-icon" />
                     <span>MOVIE</span>
                 </a>
                 <a>
-                    <img src='/images/series-icon.svg' alt="images" />
+                    <img src='/images/series-icon.svg' alt="series-icon" />
                     <span>SERIES</span>
                 </a>
             </NavMenu>
