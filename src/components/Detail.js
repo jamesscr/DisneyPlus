dcdc9fdf26b70885bd550c8d93b4a307baf4913c
jamesscr <allanjeanjacques@hotmail.com>
2021-05-02 08:44:56 -0400

    updated

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -68,6 +68,7 @@ height: 30vh;
 min-height: 170px;
 width: 35vw;
 min-width: 200px;
+margin-top: 60px;
 
 img {
     width: 100%;
@@ -140,4 +141,5 @@ const Description = styled.div`
  font-size: 20px;
  margin-top: 16px;
  color: rgb (249, 249, 249);
+ max-width: 760px;
 `
